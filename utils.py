149b0e97522ca0dfec22bf9e5f284165344ff89f
Alexandre Adjahossou <adjahossoualexandre@gmail.com>
2024-06-07 15:26:32 +0000

    Create utility function for manual result tracking.

--- utils.py
+++ utils.py
@@ -1,19 +1,27 @@
 import os
-def manual_tracking(
-        output: dict,
-        file_name: str,
-        tracking_dir: str = "manual_tracking/"
-        ) -> None:
-    file_path = tracking_dir + file_name
-    if file_name in  os.listdir(tracking_dir):
-        mode = "a"
-    else:
-        mode = "x"
-    print(mode)
-    with open(file_path, mode=mode) as f:
-        for metadata, value in output.items():
-            f.writelines(f"{metadata}: {value}")
+
+class ManualExperiment():
+
+    def __init__(self, file_name: str, dir: str, exp_metadata: dict = None) -> None:
+        self.file_name = file_name
+        self.dir = dir
+        self.exp_metadata = exp_metadata
+        self.file_path = self.dir + "/" + self.file_name
+
+        if self.exp_metadata:
+            with open(self.file_path, mode="x") as f:
+                for exp_m, value in self.exp_metadata.items():
+                    f.writelines(f"{exp_m}: {value}")
+                    f.writelines("\n")
+                f.writelines("-"*24)
+                f.writelines("\n")
+
+    def track(self, output: dict) -> None:
+        with open(self.file_path, mode="a") as f:
+            for key, value in output.items():
+                f.writelines(f"{key}: {value}")
+                f.writelines("\n")
+            f.writelines("-"*48 + "\n")
+            f.writelines("-"*48 + "\n")
             f.writelines("\n")
-        f.writelines("-"*48)
-        f.writelines("\n")
-    f.close()
+        f.close()
