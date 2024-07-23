e730d187849ff3e3cd4c02e1c76e4b22c71ee01a
Alexandre Adjahossou <adjahossoualexandre@gmail.com>
2024-07-23 08:23:55 +0000

    Adapt module to LightRAG naming.

--- metadata.py
+++ metadata.py
@@ -1,7 +1,7 @@
 import re
 
-def get_toc_info(document, first_n_char=7, chapter_name_pattern="^ch\d+"):
-    file_name: str = document.metadata["file_name"]
+def get_toc_info(document, first_n_char=7, chapter_name_pattern=r"^ch\d+"):
+    file_name: str = document.meta_data["file_name"]
 
     # retrieve substring with section/subsection info
     chapter_info = "-".join(file_name.split("-")[:2])
@@ -22,18 +22,31 @@ def get_toc_info(document, first_n_char=7, chapter_name_pattern="^ch\d+"):
 def map_chapnum_to_chapname(documents) -> dict:
     chapters = dict()
     for doc in documents:
-        chapter_number: str = doc.metadata["chapter_number"]
-        subsection_number = doc.metadata["subsection_number"]
+        chapter_number: str = doc.meta_data["chapter_number"]
+        subsection_number = doc.meta_data["subsection_number"]
 
         if isinstance(chapter_number, str):
             chapters[chapter_number] = chapter_number
         if subsection_number == 0:
-            chapters[chapter_number] =  doc.metadata["subsection_name"]
+            chapters[chapter_number] =  doc.meta_data["subsection_name"]
 
     return chapters
 
 def get_chapter_name(document, chapters: dict):
-    chapter_number: str | int = document.metadata["chapter_number"]
+    chapter_number: str | int = document.meta_data["chapter_number"]
     chapter_name = chapters[chapter_number]
 
     return chapter_name
+
+def set_metadata(documents):
+
+    # Get chapters number and subsections names & numbers
+    for document in documents:
+        infos = get_toc_info(document)
+        for key,value in infos.items():
+            document.meta_data[key] = value
+    # Get chapters names    
+    chapters = map_chapnum_to_chapname(documents)
+    for doc in documents:
+        doc.meta_data["chapter_name"] = get_chapter_name(doc, chapters)
+
