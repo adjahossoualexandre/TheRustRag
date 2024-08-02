57df22ec3d168bbfccdf1c96441a396bd34f842f
Alexandre Adjahossou <adjahossoualexandre@gmail.com>
2024-08-02 15:20:08 +0000

    Rename metadata.py -> document_metadata.py.

--- indexing.py
+++ indexing.py
@@ -1,6 +1,6 @@
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, ServiceContext
 from llama_index.core.node_parser import SentenceSplitter
-from metadata import (
+from document_metadata import (
     get_toc_info,
     get_chapter_name,
     map_chapnum_to_chapname
